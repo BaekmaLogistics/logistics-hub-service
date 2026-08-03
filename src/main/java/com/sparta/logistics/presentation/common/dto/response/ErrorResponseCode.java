21c5fcf0dce2ceaa347b91b05ff7967181cdf7ca
leunbin <silverbin2105@gmail.com>
2026-08-03 14:30:26 +0900

    feat: OpenFeign 공통 설정 추가

--- src/main/java/com/sparta/logistics/presentation/common/dto/response/ErrorResponseCode.java
+++ src/main/java/com/sparta/logistics/presentation/common/dto/response/ErrorResponseCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorResponseCode implements ApiResponseCode {
     // Common
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON_0001", "알 수 없는 오류가 발생했습니다."),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_0002","유효하지 않은 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_0002","유효하지 않은 요청입니다."),
+    FEIGN_CLIENT_ERROR(HttpStatus.BAD_GATEWAY, "COMMON_0003", "Feign 통신 중 오류가 발생했습니다.");
+
 
     private final HttpStatus status;
     private final String errorCode;
