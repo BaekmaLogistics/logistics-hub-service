05e3847cd9aa10355c6407a28d2a76262ce5993d
leunbin <silverbin2105@gmail.com>
2026-08-08 20:02:33 +0900

    Feat(Hub) : 허브 정보 조회 내부 API 구현

--- src/main/java/com/sparta/logistics/presentation/query/controller/InternalHubQueryController.java
+++ src/main/java/com/sparta/logistics/presentation/query/controller/InternalHubQueryController.java
@@ -1,23 +1,22 @@
 package com.sparta.logistics.presentation.query.controller;
 
 import com.sparta.logistics.application.query.dto.HubDetailResponse;
-import com.sparta.logistics.application.query.dto.HubSearchCondition;
 import com.sparta.logistics.application.query.usecase.HubQueryUseCase;
 import com.sparta.logistics.common.code.GeneralResponseCode;
 import com.sparta.logistics.presentation.common.dto.response.GeneralResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/hubs")
-public class HubQueryController {
+@RequestMapping("/internal/api/v1/hubs")
+public class InternalHubQueryController {
 
     private final HubQueryUseCase hubQueryUseCase;
 
@@ -28,22 +27,6 @@ public class HubQueryController {
         return GeneralResponse.toResponseEntity(
                 GeneralResponseCode.OK,
                 hubQueryUseCase.getHub(hubId)
-        );
-    }
-
-    @GetMapping
-    public ResponseEntity<GeneralResponse<Page<HubDetailResponse>>> searchHubs(
-        @ModelAttribute HubSearchCondition condition,
-        @PageableDefault(
-                page = 0,
-                size = 10,
-                sort = "createdAt"
-        ) Pageable pageable
-    ) {
-
-        return GeneralResponse.toResponseEntity(
-                GeneralResponseCode.OK,
-                hubQueryUseCase.searchHubs(condition, pageable)
         );
     }
 }
