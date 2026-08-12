c33d518c8b6fc5d75b48fcce00d934b66c3b7fc2
woohyun23 <parkwh0823@gmail.com>
2026-08-12 16:41:40 +0900

    feat: Outbox 및 RabbitMQ 공통 설정 추가

--- src/main/java/com/sparta/logistics/LogisticsApplication.java
+++ src/main/java/com/sparta/logistics/LogisticsApplication.java
@@ -4,10 +4,12 @@ import com.sparta.logistics.infrastructure.feign.config.OpenFeignConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients(
         defaultConfiguration = OpenFeignConfig.class
 )
+@EnableScheduling
 @SpringBootApplication
 public class LogisticsApplication {
 
